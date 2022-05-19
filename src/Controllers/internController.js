1ecf9a6258c480876352b52502e6d1853542ab1c
Ajay-Jaiswal <ajayjaiswal315121@gmail.com>
2022-05-19 23:33:29 +0530

    completed

---
+++ src/Controllers/internController.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+const collegeModel = require('../models/collegeModel');
+const internModel = require("../models/internModel");
+const validator = require("../util/validator")
+
+const route = require("../models/internModel");
+
+
+const createIntern = async function(req,res){
+  try{
+    let intern = req.body
+
+    const{email , mobile, name ,  collegeName} = intern;
+
+    if(!validator.isValid(name)){
+        return res.status(400).send({status:false, message: "Name is require"});
+    }
+
+
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        res.status(400).send({status: false,message: `Email should be a valid email address`});
+        return;
+      }
+
+
+      const isEmailAlredyUsed = await internModel.findOne({ email });
+      if (isEmailAlredyUsed) {
+        return res
+          .status(400)
+          .send({
+            status: false,
+            message: `${email} email address is already registered`,
+          });
+      }
+
+
+      if(intern.mobile.length !== 10){
+          return res.send({status:false , message: "mobile number is required 10 digit"})
+      }
+
+      if(!/^\+?([6-9]{1})\)?[-. ]?([0-9]{1})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/.test(mobile)){
+        return res.status(400).send({status:false, message:"give proper mobile no. in form of number"})
+      }
+      
+      let mob = await internModel.findOne({mobile: intern.mobile })
+
+      if(mob !== null){
+          return res.status(400).send({status:false, Message:`${mobile} mobile already use`})
+      }
+
+     // if (intern.collegeId.length !== 24){
+      //  return res.status(400).send({status:false, message:"invalid college Id"})
+      if(!validator.isValid(collegeName)){
+        return res.status(400).send({status:false, message: " College Name is require"});
+    }
+   // }
+   //if(!collegeName) return res.status(400).send({status:false,message:"College Name Must Require"})
+
+      let iscollegeId  = await collegeModel.findOne({name: intern.collegeName}).select({_id:1})
+      if(! iscollegeId){
+        return res.status(400).send({status:false, message: "college name not exist"})
+    }
+
+      let id=iscollegeId._id.toString()
+     intern.collegeId=id
+     delete intern.collegeName
+     // console.log(intern)
+
+
+   let internCreate = await internModel.create(intern)
+    res.status(201).send({status:true, data:internCreate})
+  }
+  catch(err){
+    res.status(500).send({status:false, message: err.message})
+  }
+}
+
+module.exports.createIntern = createIntern;
---
+++ src/route/route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { route } = require('express/lib/application');
+const router = express.Router();
+
+const collegeController = require("../Controllers/collegeController")
+const internController = require("../Controllers/internController")
+
+router.post('/functionup/colleges', collegeController.createCollege)
+router.post('/functionup/interns', internController.createIntern)
+router.get("/functionup/collegeDetails", collegeController.getcollegeDetails)
+
+
+
+module.exports = router;
